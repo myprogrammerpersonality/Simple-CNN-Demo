ccb66e577497a8a96f8bac9b409700b823ba3905
Ali_Yazdizadeh <aliyzd1379@gmail.com>
2023-08-15 21:22:49 +0330

    new model

--- assets/main.py
+++ assets/main.py
@@ -4,11 +4,10 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from mangum import Mangum
 import torchvision
-from torchvision.models.detection import fasterrcnn_mobilenet_v3_large_320_fpn
+from torchvision.models.detection import FasterRCNN_ResNet50_FPN_Weights
 import torch
-from PIL import Image
 import cv2
-import io
+import base64
 import numpy as np
 import os
 
@@ -18,11 +17,25 @@ templates = Jinja2Templates(directory="templates")
 
 # Set TORCH_HOME to the same directory as in the Dockerfile
 os.environ["TORCH_HOME"] = "/var/task/torch_cache"
-model = fasterrcnn_mobilenet_v3_large_320_fpn(pretrained=True)
+model = torchvision.models.detection.fasterrcnn_resnet50_fpn(weights=FasterRCNN_ResNet50_FPN_Weights.DEFAULT)
 model.eval()
 
-def predict(img_bytes):
-    img = Image.open(io.BytesIO(img_bytes)).convert("RGB")
+COCO_LABELS = [
+    'N/A', 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
+    'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'N/A',
+    'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse',
+    'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'N/A', 'backpack',
+    'umbrella', 'N/A', 'N/A', 'handbag', 'tie', 'suitcase', 'frisbee', 'skis',
+    'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard',
+    'surfboard', 'tennis racket', 'bottle', 'N/A', 'wine glass', 'cup', 'fork', 'knife',
+    'spoon', 'bowl', 'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
+    'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'N/A', 'dining table', 'N/A', 'N/A', 'toilet', 'N/A',
+    'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone', 'microwave', 'oven', 'toaster',
+    'sink', 'refrigerator', 'N/A', 'book', 'clock', 'vase', 'scissors', 'teddy bear', 'hair drier',
+    'toothbrush'
+]
+
+def predict(img):
     tensor = torch.unsqueeze(torchvision.transforms.functional.to_tensor(img), 0)
     with torch.no_grad():
         prediction = model(tensor)
@@ -35,23 +48,37 @@ async def chatbot_page(request: Request):
 @app.post("/uploadfile/", response_class=HTMLResponse)
 async def upload_file(request: Request, file: UploadFile = File(...)):
     try:
-        img_bytes = await file.read()
-        img = cv2.imdecode(np.frombuffer(img_bytes, np.uint8), cv2.IMREAD_COLOR)
-        
+        contents = await file.read()
+        nparr = np.frombuffer(contents, np.uint8)
+        img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+        print(img.shape)
+
         if img is None:
             raise HTTPException(status_code=400, detail="Unable to decode image")
 
         prediction = predict(img)
-        
-        for box, score in zip(prediction[0]['boxes'], prediction[0]['scores']):
+
+        print("^^", prediction)
+
+        if not prediction:
+            raise HTTPException(status_code=400, detail="Prediction failed")
+
+        for box, label, score in zip(prediction[0]['boxes'], prediction[0]['labels'], prediction[0]['scores']):
             if score > 0.5:
+                label_text = f"{COCO_LABELS[label.item()]}: {score.item():.2f}"
+                print(label_text)
                 cv2.rectangle(img, (int(box[0]), int(box[1])), (int(box[2]), int(box[3])), (255, 0, 0), 2)
+                cv2.putText(img, label_text, (int(box[0]), int(box[1])-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
+
 
         _, img_byte_arr = cv2.imencode('.png', img)
+        img_base64 = base64.b64encode(img_byte_arr.tobytes()).decode()
+        img_data_url = f"data:image/png;base64,{img_base64}"
         
-        return templates.TemplateResponse("base.html", {"request": request, "image": img_byte_arr.tobytes()})
+        return templates.TemplateResponse("base.html", {"request": request, "image": img_data_url})
 
     except Exception as e:
+        print(f"Error: {str(e)}") # print or log the error for debugging
         raise HTTPException(status_code=400, detail=str(e))
 
 @app.get("/ping")
