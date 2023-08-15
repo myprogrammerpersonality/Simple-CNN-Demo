5050f2dfff055298fa494dd6437f28a3380ccc28
Ali_Yazdizadeh <aliyzd1379@gmail.com>
2023-08-15 18:13:39 +0330

    change stack name

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from stacks.app_stack import AppStack
 
 app = cdk.App()
 
-AppStack(app, "AppStack")
+AppStack(app, "CNNAppStack")
 
 cdk.Tags.of(app).add(key="owner", value="ali@datachef.co")
 cdk.Tags.of(app).add(key="application", value="FastAPI")
